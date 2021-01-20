ce51a72890083a68c14f3c692af824424b140500
kwak <ksh03003@naver.com>
2021-01-20 14:43:55 +0900

    day7

--- 1. 장고란 무엇인가/firstProject1/blogapp/forms.py
+++ 1. 장고란 무엇인가/firstProject1/blogapp/forms.py
@@ -1,9 +1,18 @@
 from django import forms
 from .models import Blog
-
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
 
 # 블로그 생성
 class CreateBlog (forms.ModelForm):
     class Meta:
         model = Blog
         fields = ['title', 'body', 'author']
+        widgets = {
+            'title': forms.TextInput(
+                attrs={'class' : 'form-control', 'style': 'width:100%','placeholder':'제목을 입력하세요.'}
+            ),
+            'author': forms.Select(
+                attrs={'class' : 'custom-select'}
+            ),
+            'body': forms.CharField(widget=CKEditorUploadingWidget()),
+        }
